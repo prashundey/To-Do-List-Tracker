dae2dde4934bf7bada2adefa2ab1f2f3488cd6b2
Prashun Dey <prashun.dey@stonybrook.edu>
2021-01-28 17:41:31 -0500

    UI update

--- src/com/dey/todolist/Main.java
+++ src/com/dey/todolist/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml"));
         primaryStage.setTitle("To Do List");
-        primaryStage.setScene(new Scene(root, 900, 500));
+        primaryStage.setScene(new Scene(root, 600, 500));
         primaryStage.show();
     }
 
